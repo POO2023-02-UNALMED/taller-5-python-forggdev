e704bdedaff9685176a38f9fd5a28a497eb9c3f3
forggdev <141962334+forggdev@users.noreply.github.com>
2023-09-19 17:01:02 -0500

    Update v1.4
    Changed:
    animal.py

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -22,7 +22,7 @@ class Animal:
         from zooAnimales.reptil import Reptil
         from zooAnimales.pez import Pez
         from zooAnimales.anfibio import Anfibio
-        return "Mamiferos:", str(Mamifero.cantidadMamiferos()), "\nAves:", str(Ave.cantidadAves()), "\nReptiles:", str(Reptil.cantidadReptiles()), "\nPeces:", str(Pez.cantidadPeces()), "\nAnfibios:", str(Anfibio.cantidadAnfibios())
+        return str("Mamiferos:", str(Mamifero.cantidadMamiferos()), "\nAves:", str(Ave.cantidadAves()), "\nReptiles:", str(Reptil.cantidadReptiles()), "\nPeces:", str(Pez.cantidadPeces()), "\nAnfibios:", str(Anfibio.cantidadAnfibios()))
     
     '''def __str__(self):
         if self._zona == None:
@@ -32,9 +32,9 @@ class Animal:
         
     def toString(self):
         if self._zona == None:
-            return "Mi nombre es",  self._nombre, "tengo una edad de", self._edad, "habito en", self._habitat, "y mi genero es", self._genero
+            return str("Mi nombre es",  self._nombre, "tengo una edad de", self._edad, "habito en", self._habitat, "y mi genero es", self._genero)
         else:
-            return "Mi nombre es",  self._nombre, "tengo una edad de", self._edad, "habito en", self._habitat, "y mi genero es", self._genero, "la zona en la que me ubico es", self._zona, "en el", self._zona.getZoo()
+            return str("Mi nombre es",  self._nombre, "tengo una edad de", self._edad, "habito en", self._habitat, "y mi genero es", self._genero, "la zona en la que me ubico es", self._zona, "en el", self._zona.getZoo())
 
         
     # Set and get methods
